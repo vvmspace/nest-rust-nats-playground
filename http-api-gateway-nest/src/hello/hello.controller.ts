2fea45450c52db72e8e04accd6a17256baa8367b
Vladimir V.M <vvmyagdeev@gmail.com>
2024-02-22 15:51:16 +0400

    feat<api>: add swagger

--- http-api-gateway-nest/src/hello/hello.controller.ts
+++ http-api-gateway-nest/src/hello/hello.controller.ts
@@ -1,6 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
+import {ApiOperation, ApiTags} from "@nestjs/swagger";
 @Controller()
 export class HelloController {
+  @ApiTags('Hello')
+  @ApiOperation({ summary: 'Hello NATS' })
   @Get()
   helloNATS(): string {
     return 'Hello NATS!';
